428aa3e26817e2cb4f1cf490cdd8a206d16f6ce4
Purple-Snake <titas.sniuksta@gmail.com>
2023-09-14 14:11:45 +0300

    added react bootstrap
    and a card to fooditems

--- Frontend/src/components/FoodItems.jsx
+++ Frontend/src/components/FoodItems.jsx
@@ -1,11 +1,15 @@
-
+import Card from 'react-bootstrap/Card';
 
 function FoodItems( foodItem ) {
     console.log(foodItem);
     return ( 
-        <div key={foodItem.foodItem.id}>
-            
-        </div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{foodItem.foodItem.name}</Card.Title>
+          <Card.Text>{foodItem.foodItem.ingredients}</Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+        </Card.Body>
+      </Card>
      );
 }
 
