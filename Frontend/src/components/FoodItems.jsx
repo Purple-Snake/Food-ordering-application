186b1c5cad94676b4ae3dc2e6ac795c76cc06498
Purple-Snake <titas.sniuksta@gmail.com>
2023-09-14 14:21:49 +0300

    added svg and png folders
    and moved all of the svg to the folder

--- Frontend/src/components/FoodItems.jsx
+++ Frontend/src/components/FoodItems.jsx
@@ -1,16 +1,17 @@
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
-function FoodItems( foodItem ) {
-    console.log(foodItem);
-    return ( 
-        <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>{foodItem.foodItem.name}</Card.Title>
-          <Card.Text>{foodItem.foodItem.ingredients}</Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-        </Card.Body>
-      </Card>
-     );
+function FoodItems(foodItem) {
+  console.log(foodItem);
+  return (
+    <Card style={{ width: "70rem" }}>
+      <Card.Body>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Title>{foodItem.foodItem.name}</Card.Title>
+        <Card.Text>{foodItem.foodItem.ingredients}</Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default FoodItems;
