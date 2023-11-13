8fb716ab72671a4a21ebc2bc13d7a116c9b32f31
Purple-Snake <titas.sniuksta@gmail.com>
2023-11-13 14:34:43 +0200

    removed test code

--- Backend/app.js
+++ Backend/app.js
@@ -25,31 +25,4 @@ app.use("/menu", menuRouter)
 app.use("/auth", userRouter)
 app.use("/order", orderRouter)
 
-
-// const multer = require("multer");
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "./uploads/");
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now();
-//     cb(null, uniqueSuffix + file.originalname);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// app.post("/upload-image", upload.single("image"), async (req, res) => {
-//   console.log(req.body);
-//   const imageName = req.file.filename;
-
-//   try {
-//     await Images.create({ image: imageName });
-//     res.json({ status: "ok" });
-//   } catch (error) {
-//     res.json({ status: error });
-//   }
-// });
-
 module.exports = app;
