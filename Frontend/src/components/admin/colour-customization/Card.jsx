6a0909207526956b50ecae63a12cce22a2cbdc99
PinkyReox <144027379+PinkyReox@users.noreply.github.com>
2023-11-21 15:40:28 +0200

    commiteee

--- Frontend/src/components/admin/colour-customization/Card.jsx
+++ Frontend/src/components/admin/colour-customization/Card.jsx
@@ -9,10 +9,16 @@ function Card() {
       {Object.entries(colourValues).map((entry) => {
         let key = entry[0];
         let value = entry[1];
+
+                // Exclude ObjectID from rendering
+                if (key === '_id') {
+                  return null;
+                }
+                
         return (
           <div key={key}>
             <label htmlFor={`${key}-colour`}>{key} colour:</label>
-            <input type="text" id={`${key}-colour`} value={value} />
+            <input type="text" id={`${key}-colour`} defaultValue={value} />
           </div>
         );
       })}
