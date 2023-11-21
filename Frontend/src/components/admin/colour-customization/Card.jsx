df71a93f2f54d26b754d6a93eeda901172a5e716
Purple-Snake <titas.sniuksta@gmail.com>
2023-11-21 14:02:11 +0200

    aaa

--- Frontend/src/components/admin/colour-customization/Card.jsx
+++ Frontend/src/components/admin/colour-customization/Card.jsx
@@ -2,22 +2,22 @@ import { useContext } from "react";
 import { CustomizationContext } from "../../../context/CustomizationContext";
 
 function Card() {
-    const { colourValues } = useContext(CustomizationContext)
+  const { colourValues } = useContext(CustomizationContext);
 
-    return ( 
-        <div>
-            <div>
-                <label htmlFor="Background-colour">Background colour:</label>
-                <input type="text" />
-                <label htmlFor="primary-colour">Primary colour:</label>
-                <input type="text" />
-                <label htmlFor="Secondary-colour">Secondary colour:</label>
-                <input type="text" />
-                <label htmlFor=""> colour:</label>
-                <input type="text" />
-            </div>
-        </div>
-     );
+  return (
+    <div>
+      {Object.entries(colourValues).map((entry) => {
+        let key = entry[0];
+        let value = entry[1];
+        return (
+          <div key={key}>
+            <label htmlFor={`${key}-colour`}>{key} colour:</label>
+            <input type="text" id={`${key}-colour`} value={value} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Card;
