f2a43b4237d6124b1e30da8410a32e3c664cd68b
Purple-Snake <titas.sniuksta@gmail.com>
2023-11-21 15:37:40 +0200

    Colours

--- Frontend/src/context/CustomizationContext.jsx
+++ Frontend/src/context/CustomizationContext.jsx
@@ -14,12 +14,22 @@ export const CustomizationContextProvider = ({ children }) => {
     })
   }
 
+  function setColours() {
+    const root = document.querySelector(":root")
+
+    root.style.setProperty("--background-color", `${colourValues.background_colour}`)
+    root.style.setProperty("--Primary", `${colourValues.primary_colour}`);
+    root.style.setProperty("--Secondary", `${colourValues.secondary_colour}`)
+    // root.style.setProperty("", `${colourValues._colour}`)
+  }
+
   useEffect(() => {
-    getColours()
-  }, [])
+    getColours().then(() => setColours())
+  }, [])  
 
   useEffect(() => {
     console.log(colourValues);
+    console.log(colourValues.background_colour);
   }, [colourValues])
     
 
