2895f718c87e918a1c109f50fcf31174b8ce3794
PinkyReox <144027379+PinkyReox@users.noreply.github.com>
2023-11-23 16:33:29 +0200

    preview color update

--- Frontend/src/components/admin/colour-customization/Card.jsx
+++ Frontend/src/components/admin/colour-customization/Card.jsx
@@ -1,13 +1,42 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { CustomizationContext } from "../../../context/CustomizationContext";
 import axios from "axios";
+import "./card.css";
 
 function Card() {
   const { colourValues, getColours } = useContext(CustomizationContext);
-  const [hexValue, setHexValue] = useState("");
   const _id = colourValues._id;
+  const [hexValue, setHexValue] = useState({});
 
-  // Tried the preview square but doesn't work
+  useEffect(() => {
+    // Set initial hex values when component mounts
+    const initialHexValues = {};
+    Object.keys(colourValues).forEach((key) => {
+      if (key !== "_id") {
+        initialHexValues[key] = colourValues[key];
+      }
+    });
+    setHexValue(initialHexValues);
+  }, [colourValues]);
+
+  function handleSumbitPatch(e) {
+    e.preventDefault();
+
+    const updatedColours = { _id };
+
+    e.target.querySelectorAll(".colour-input").forEach((input) => {
+      const key = input.id;
+      updatedColours[key] = input.value;
+    });
+
+    try {
+      axios.patch("http://localhost:3000/custom/updateColours", updatedColours);
+      getColours();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   function handleHexChange(key, hex) {
     setHexValue((prevHexValue) => ({
       ...prevHexValue,
@@ -15,60 +44,42 @@ function Card() {
     }));
   }
 
-  useEffect(() => {
-    console.log(hexValue);
-  }, [hexValue]);
-
-  async function handleSumbitPatch(e) {
-    e.preventDefault();
-
-    const updatedColours = { _id };
-
-    e.target.querySelectorAll(".colour-input").forEach((input) => {
-      updatedColours[input.id] = input.value;
-    });
-
-    try {
-      await axios.patch(
-        "http://localhost:3000/custom/updateColours",
-        updatedColours
-      );
-      getColours();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   return (
     <div className="custom-colour-panel flex-col m-auto mt-[10%] p-5 w-[25rem]">
       <form onSubmit={handleSumbitPatch}>
-        {Object.entries(colourValues).map((entry) => {
-          let key = entry[0];
-          let value = entry[1];
-
-          // Exclude ObjectID from rendering
+        {Object.keys(colourValues).map((key) => {
           if (key === "_id") {
             return null;
           }
 
+          const value = colourValues[key];
+
           return (
             <div key={key}>
-              <label htmlFor={`${key}`}>{key}:</label>
+              <label htmlFor={key}>{key}:</label>
               <input
                 className="colour-input"
                 type="text"
-                id={`${key}`}
+                id={key}
                 defaultValue={value}
-                maxLength={7}
                 onChange={(e) => handleHexChange(key, e.target.value)}
               />
-              <span className={`w-[20px] h-[20px] bg-[${hexValue[key]}]`}>
-                AAA
+              <span className="color-preview" style={{width: '20px', height: '20px', padding: '0.3rem', backgroundColor: hexValue[key], border: '1px solid #000', borderRadius: '5px' }}>
+                AaBb
               </span>
             </div>
           );
         })}
-        <input type="submit" value="Update colours" />
+        <input 
+        type="submit" 
+        value="Update colours" 
+        style={{
+          border: '1px solid #000',
+          cursor: 'pointer',
+          padding: '0.3rem 0.5rem',
+          borderRadius: '5px',
+        }}
+      />
       </form>
     </div>
   );
