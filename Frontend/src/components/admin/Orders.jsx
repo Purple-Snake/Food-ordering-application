321a70a56ca41356b1174f99317ddaff89dc09d2
Purple-Snake <titas.sniuksta@gmail.com>
2023-11-27 16:33:32 +0200

    added timer to the orders

--- Frontend/src/components/admin/Orders.jsx
+++ Frontend/src/components/admin/Orders.jsx
@@ -1,24 +1,24 @@
 import { useContext } from "react";
-import Order from "./Order"
+import Order from "./Order";
 import { AdminContext } from "../../context/AdminContext";
 
 function Orders() {
-    const { orderData, isLoading } = useContext(AdminContext)
+  const { orderData, isLoading } = useContext(AdminContext);
 
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
-    return ( 
-        <div className="order-list-container">
-        <div className="order-list flex-col">
-            {orderData.map((order) => (
-                <div className="m-5" key={order._id}>
-                    <Order order={order} />
-                </div>
-            ))}
-        </div>
-        </div>
-     );
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div className="order-list-container">
+      <div className="order-list flex-col">
+        {orderData.map((order) => (
+          <div className="m-5" key={order._id}>
+            <Order order={order} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
